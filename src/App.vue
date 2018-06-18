d3d67be2389255f1bab16b6b1fef0e9d6cffc380
divya <hello@shortdiv.com>
2018-06-18 16:20:44 +0800

    feat: show order based on option selected

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <KopiOptions />
+    <KopiOrder
+      :milk="milk"
+    />
   </div>
 </template>
 
 <script>
 import KopiOptions from './components/KopiOptions.vue'
+import KopiOrder from './components/KopiOrder.vue'
 
 export default {
   name: 'app',
   components: {
-    KopiOptions
+    KopiOptions,
+    KopiOrder
   }
 }
 </script>
---
+++ src/components/KopiOrder.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ milk }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'KopiOrder',
+  props: {
+    milk: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
