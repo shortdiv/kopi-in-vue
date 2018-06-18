af192c50ad5e182360adb93dd2ffc20de724d02c
divya <hello@shortdiv.com>
2018-06-18 16:09:00 +0800

    feat: create radio buttons for milk

---
+++ src/components/KopiOptions.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li class="milk-options">
+      <label
+        v-for="(milkType,index) in milkTypes"
+        :key="index"
+      >
+        <input type="radio" name="milk">
+        <span>{{ milkType }}</span>
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'KopiOptions',
+  data () {
+    return {
+      milkTypes: [
+        'no milk',
+        'condensed milk',
+        'evaporated milk'
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+  li {
+    list-style: none;
+  }
+</style>
